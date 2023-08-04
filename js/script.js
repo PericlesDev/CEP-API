bb96494aabd4ef211f1bd56c7d7e7523e8b09a07
Péricles dos Santos <91090285+PericlesDev@users.noreply.github.com>
2023-08-03 22:31:40 -0300

    Validate - Validando o CEP, e os campos do input no Javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log("Deu tudo certo!")
+const  addressForm = document.querySelector("#address-form");
+const cepInput = document.querySelector("#cep");
+const addressInput = document.querySelector("#address");
+const cityInput = document.querySelector("#city");
+const neighborhoodInput = document.querySelector("#neighborhood");
+const regionInput = document.querySelector("#region");
+const formInputs = document.querySelector("[data-input]");
+
+const closeButton = document.querySelector("#close-message");
+
+//Validate CEP input
+cepInput.addEventListener("keypress", (e) => {
+    const onlyNumbers = /[0-9]/;
+    const key = String.fromCharCode(e.keyCode);
+
+    // allow only numbers
+    if (!onlyNumbers.test(key)) {
+        e.preventDefault();
+        return;
+    }
+});
